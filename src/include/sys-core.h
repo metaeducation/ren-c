0e14f949b188fd6bcf68d853ded208d391582bd6
Hostile Fork <brian@hostilefork.com>
2015-07-31 19:33:38 -0400

    Make REBDOF take a const pointer to function-valued type
    This change helps reduce the likelihood of an accident when passing the
    new REBVAL* argument to the DO routines, in distinguishing it from the
    REBVAL* holding the function by being mutable.
    
    It was a little surprising that ROUTINE! when dispatched modifies a series
    kept inside the REBVAL* for the routine on each call.  I've reported this to
    @ShixinZeng as a problem for threading (if and when that ever happens)
    so there's a mutability cast there for now to keep track of the issue.

--- src/include/sys-core.h
+++ src/include/sys-core.h
@@ -683,7 +683,7 @@ typedef struct rebol_time_fields {
 
 
 // DO evaltype dispatch function
-typedef void (*REBDOF)(REBVAL *ds);
+typedef void (*REBDOF)(const REBVAL *ds);
 
 
 /***********************************************************************
