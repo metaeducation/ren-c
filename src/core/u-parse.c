8d2b6a2e388e9e396e7ae6173c32e3a433fc8f8f
Giulio Lunati <giuliolunati@gmail.com>
2015-08-30 22:24:30 +0200

    Bugfix.

--- src/core/u-parse.c
+++ src/core/u-parse.c
@@ -312,6 +312,7 @@ no_result:
 ***********************************************************************/
 {
 	REBSER *series = parse->series;
+	REBSER *ser;
 	REBCNT type = parse->type;
 	REBVAL *blk;
 	const REBVAL *item;
@@ -412,14 +413,15 @@ no_result:
 					ch2 = '<';
 					if (ch1 == ch2) {
 						// adapted from function Parse_To :-)
-						REBSER *ser;
 						ser = Copy_Form_Value(item, 0);
 						i = Find_Str_Str(series, 0, index, series->tail, 1, ser, 0, ser->tail,  AM_FIND_MATCH | parse->flags);
 						if (i != NOT_FOUND) {
 							if (is_thru) i += ser->tail;
 							index = i;
+							Free_Series(ser);
 							goto found;
 						}
+						Free_Series(ser);
 					}
 				}
 				else if (ANY_STR(item)) {
