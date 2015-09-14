76dfd026770d0f25b3463b7256a12de623f18b90
Giulio Lunati <giuliolunati@gmail.com>
2015-09-14 16:04:02 +0200

    Enable Android 5 build
    Requires -fPIE/fPIC in compiler flags and -pie in linker flags.

--- src/tools/systems.r
+++ src/tools/systems.r
@@ -89,7 +89,7 @@ systems: [
 			[LEN LLC +O1 ST1 -LM]
 	;-------------------------------------------------------------------------
 	0.13.01		android-arm		android
-			[LEN LLC HID F64 LDL LLOG -LM]
+			[LEN LLC HID F64 LDL LLOG -LM PIE PIC]
 	;-------------------------------------------------------------------------
 	0.14.01		syllable-dtp	posix
 			[LEN LLC +O2 HID LDL ST1 -LM LC25]
@@ -143,6 +143,7 @@ linker-flags: context [
 	LC25: ""						; libc 2.5
 	LC211: ""						; libc 2.11
 	LCB: ""							; bionic (Android)
+	PIE: "-pie"
 ]
 
 other-flags: context [
