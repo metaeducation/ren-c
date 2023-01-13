abff70ad980dd86a821eaf5584355e09d67ef222
Brian Dickens <brian@hostilefork.com>
2023-01-13 14:12:33 -0500

    Add code for unplugging and replugging stacks
    This modernizes a service routine from the old stackless
    branch, which makes it possible to detach a series of
    stack frames and then plug those stack frames in later.
    
    It has to preserve the mold buffer and the data stack
    in order to do this.

--- src/core/c-state.c
+++ src/core/c-state.c
@@ -96,6 +96,221 @@ void Rollback_Globals_To_State(struct Reb_State *s)
 }
 
 
+#define PLUG_FLAG_HAS_DATA_STACK SERIES_FLAG_24
+#define PLUG_FLAG_HAS_MOLD SERIES_FLAG_25
+
+
+//
+//  Unplug_Stack: C
+//
+// Pulls a stack out into an independent list of frames, subtracting out the
+// base frame as a baseline.  The resulting frame stack will end in nullptr
+// (instead of BOTTOM_FRAME).  You can then replug with Replug_Stack, e.g.
+// the following should be a no-op:
+//
+//      Frame(*) base = frame_->prior->prior;
+//      assert(frame_->prior != nullptr);
+//
+//      Unplug_Stack(SPARE, frame_, base);
+//
+//      assert(frame_->prior == nullptr);
+//      assert(TOP_FRAME == base);
+//
+//      Replug_Stack(frame_, TOP_FRAME, SPARE);
+//
+// This is used by something like YIELD, which unplugs the series of frames
+// all the way up to the GENERATOR (or YIELDER) that it's running under...
+// restoring the stack so the generator is back on top and able to return
+// a value.  Any global state (like mold buffer bits or the data stack) which
+// may not be at the same point when the replug happens is moved into a
+// cell managed by the caller.  This is referred to as a "plug".
+//
+void Unplug_Stack(
+    Value(*) plug,  // cell where global state differentials can be stored
+    Frame(*) f,  // frame to unplug (currently can only unplug topmost frame)
+    Frame(*) base  // base frame to unplug relative to
+){
+    assert(f == TOP_FRAME);
+
+    Frame(*) temp = f;
+    while (true) {
+        if (Get_Frame_Flag(temp, ROOT_FRAME)) {
+            //
+            // !!! Handling errors in stackless is still a work in progress;
+            // avoid confusion on this case by asserting for now.
+            //
+            assert(!"Can't yield across non-continuation-frame");
+            fail ("Cannot yield across frame that's not a continuation");
+        }
+
+        if (temp->out == base->out) {
+            //
+            // Reassign to mark the output as something randomly bad, but
+            // still GC safe.  When the stack gets patched back in, it will
+            // be recognized and reset to the new base's out.
+            //
+            temp->out = m_cast(REBVAR*, Lib(TRUE));
+        }
+        else if (temp->out == &base->spare) {
+            temp->out = m_cast(REBVAR*, Lib(FALSE));
+        }
+
+        // We make the baseline stack pointers in each frame relative to the
+        // base frame, with that frame as if it were 0.  When the frame
+        // gets plugged in again, we'll add the new base's dsp back in.
+        //
+        // !!! This may confuse a fail() if it expects to climb the stack and
+        // see all the f->baseline.dsp be sane.  But as far as interim state
+        // is concerned, there's no good number to put here...leaving it as
+        // it was would be wrong too.  This might suggest an EVAL_FLAG for
+        // "don't believe the dsp".  Tricky.
+        //
+        temp->baseline.stack_base -= base->baseline.stack_base;
+
+        if (temp->prior == base) {
+            //
+            // The frame below the base was not fulfilling an argument, it
+            // should be writing into the base's out cell.  But when the
+            // base goes off the stack, that cell will most likely be
+            // gone.  We'll have to point it at the new base's out cell
+            // when we plug it back in.  Also we have to set it to something
+            // legal to mark in GC as the cell will go stale.
+            //
+            assert(FRM_STATE_BYTE(temp->prior) != 0);  // must be a continuation
+
+            // !!! This is true for YIELD's relationship to the YIELDER, but
+            // why would it be generically true?
+            //
+            /* assert(temp->out == Lib(TRUE)); */  // should have matched base
+
+            temp->prior = nullptr;  // show where the fragment of stack ends
+            break;
+        }
+
+        temp = temp->prior;
+
+        if (temp == TOP_FRAME)  // "alive", but couldn't find in the stack walk
+            fail ("Cannot yield to a generator that is suspended");
+
+        assert(FRM_STATE_BYTE(temp) != 0);  // must be a continuation
+    }
+
+    // If any data stack has been accrued, we capture it into an array.  We
+    // will have to re-push the values when the frame is plugged back in.
+    //
+    // !!! We do not technically need to manage this array...just keep the
+    // values in it alive during GC.  But for simplicity, we keep it in a
+    // value cell, and manage it.
+    //
+    Flags flags = FLAG_FLAVOR(PLUG);  // be agnostic, to be generic!
+
+    if (STR_SIZE(STR(MOLD_BUF)) > base->baseline.mold_buf_size) {
+        flags |= PLUG_FLAG_HAS_MOLD;
+        Init_Text(
+            PUSH(),
+            Pop_Molded_String_Core(
+                STR(MOLD_BUF),
+                base->baseline.mold_buf_size,
+                base->baseline.mold_buf_len
+            )
+        );
+    }
+
+    if (TOP_INDEX > base->baseline.stack_base)  // do first (other flags push)
+        flags |= PLUG_FLAG_HAS_DATA_STACK;
+
+    if (flags == FLAG_FLAVOR(PLUG))
+        Init_Block(plug, EMPTY_ARRAY);
+    else
+        Init_Block(
+            plug,
+            Pop_Stack_Values_Core(base->baseline.stack_base, flags)
+        );
+
+    TG_Top_Frame = base;
+}
+
+
+//
+//  Replug_Stack: C
+//
+// This reverses the process of Unplug_Stack, patching a stack onto a new
+// base location.
+//
+// 1. The top frame for base at unplug time may have targeted any output cell.
+//    That output is likely gone (an argument fulfillment for a now-finished
+//    function, an API cell that was released, etc.)  But more frames than
+//    that could have inherited the same f->out.
+//
+//    Unplug_Stack() put a bogus pointer to the read-only Lib(TRUE) cell, which
+//    is good enough to be GC safe and also distinct.  Anywhere we see that,
+//    replace with the output this new base wants to write its output to.
+//
+// 2. Unplug made the stack_base be relative to 0.  We're going to restore the
+//    values that were between the base and the unplugged frame on the data
+//    stack.  But that means we have to touch up the `stack_base` pointers as
+//    well in the frames.
+//
+// 3. We chain the stack that was underneath the old base to the new base so
+//    that it now considers this base the parent.  We also update the outputs
+//    of that subframe to match the output of the current frame (see assert in
+//    Unplug_Stack() proving subframe had same f->out).
+//
+void Replug_Stack(Frame(*) f, Frame(*) base, Value(*) plug) {
+    assert(base == TOP_FRAME);  // currently can only plug atop topmost frame
+
+    Frame(*) temp = f;
+    while (true) {
+        if (temp->out == Lib(TRUE))  // replace output placeholder, see [1]
+            temp->out = base->out;
+        else if (temp->out == Lib(FALSE))
+            temp->out = cast(REBVAL*, &base->spare);
+
+        temp->baseline.stack_base += base->baseline.stack_base;  // see [2]
+
+        if (temp->prior == nullptr)
+            break;
+        temp = temp->prior;
+       /* assert(FRM_STATE_BYTE(temp) != 0); */ // must be continuation (why?)
+    }
+
+    temp->prior = base;
+
+    // Now add in all the residual elements from the plug to global buffers
+    // like the mold buffer and data stack.
+
+    assert(IS_BLOCK(plug));  // restore data stack from plug's block
+    assert(VAL_INDEX(plug) == 0);  // could store some number (?)
+
+    if (VAL_ARRAY(plug) == EMPTY_ARRAY)
+        goto finished;
+
+  blockscope {
+
+    Array(*) array = VAL_ARRAY_KNOWN_MUTABLE(plug);
+    Cell(*) item = ARR_TAIL(array);
+
+    if (Get_Subclass_Flag(PLUG, array, HAS_MOLD)) {  // restore mold from plug
+        --item;
+        assert(IS_TEXT(item));
+        assert(VAL_INDEX(item) == 0);
+        Append_String(STR(MOLD_BUF), item);
+    }
+
+    if (Get_Subclass_Flag(PLUG, array, HAS_DATA_STACK)) {
+        Cell(*) stacked = ARR_HEAD(array);
+        for (; stacked != item; ++stacked)
+            Move_Cell(PUSH(), SPECIFIC(stacked));
+    }
+
+} finished: {
+
+    Init_Trash(plug);  // no longer needed, let it be GC'd
+
+    TG_Top_Frame = f;  // make the jump deeper into the stack official...
+}}
+
+
 #if !defined(NDEBUG)
 
 //
