853594f40837c8f7c96d3bfdab1381b487350e57
Brian Dickens <brian@hostilefork.com>
2023-09-20 17:20:57 -0400

    Make LOGIC! legal return type from JS-EVAL
    Return types were not checked previously if they were isotopes.
    Now that they are, LOGIC! has to be mentioned.

--- extensions/javascript/mod-javascript.c
+++ extensions/javascript/mod-javascript.c
@@ -965,7 +965,7 @@ DECLARE_NATIVE(js_native)
 //  {Evaluate textual JavaScript code}
 //
 //      return: "Note: Only supports types that reb.Box() supports"
-//          [<opt> <none> integer! text!]
+//          [<opt> <none> logic! integer! text!]
 //      source "JavaScript code as a text string" [text!]
 //      /local "Evaluate in local scope (as opposed to global)"
 //      /value "Return a Rebol value"
