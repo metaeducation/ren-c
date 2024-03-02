rhencke/rebol

Fix latent Do_Any_Array_At_Throws(D_OUT, D_OUT) use
At one time, it was apparently safe to use the same value as the input
and the output to Do_Any_Array_At_Throws().  But when it was changed
to do Init_Void(D_OUT) in its implementation as part of the function
arguments, that changed...and depending on argument evaluation order,
it could try to extract a VAL_ARRAY() on a void...which was an error.

src/core/n-control.c
@@ -764,15 +764,15 @@ static REB_R Case_Choose_Core_May_Throw(
 
             f->gotten = nullptr; // can't hold onto cache, running user code
 
-            if (IS_BLOCK(D_OUT)) {
-                if (Do_Any_Array_At_Throws(D_OUT, D_OUT)) { // legal args
+            Move_Value(block, D_OUT); // can't evaluate into ARG(block)
+            if (IS_BLOCK(block)) {
+                if (Do_Any_Array_At_Throws(D_OUT, block)) {
                     Abort_Frame(f);
                     DROP_GC_GUARD(cell);
                     return R_THROWN;
                 }
             }
             else if (IS_ACTION(D_OUT)) {
-                Move_Value(block, D_OUT); // can't evaluate into ARG(block)
                 if (Do_Branch_With_Throws(D_OUT, block, cell)) {
                     Abort_Frame(f);
                     DROP_GC_GUARD(cell);

metaeducation/ren-c

src/core/n-control.c
@@ -672,15 +672,15 @@ static REB_R Case_Choose_Core_May_Throw(
 
             f->gotten = nullptr; // can't hold onto cache, running user code
 
-            if (IS_BLOCK(OUT)) {
-                if (Do_Any_Array_At_Throws(OUT, OUT)) { // legal args
+            Move_Value(block, OUT); // can't evaluate into ARG(block)
+            if (IS_BLOCK(block)) {
+                if (Do_Any_Array_At_Throws(OUT, block)) {
                     Abort_Frame(f);
                     DROP_GC_GUARD(cell);
                     return R_THROWN;
                 }
             }
             else if (IS_ACTION(OUT)) {
-                Move_Value(block, OUT); // can't evaluate into ARG(block)
                 if (Do_Branch_With_Throws(OUT, block, cell)) {
                     Abort_Frame(f);
                     DROP_GC_GUARD(cell);