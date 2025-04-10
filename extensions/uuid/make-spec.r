e4777b4c23f54da96abc21d18718a3ee8cb6596f
Brian Dickens <brian@hostilefork.com>
2025-04-10 13:38:57 -0700

    "CoreFoundation" in a block in uuid module
    Currently libraries need to be a block.  Review.

--- extensions/uuid/make-spec.r
+++ extensions/uuid/make-spec.r
@@ -33,6 +33,6 @@ libraries: switch platform-config.os-base [
 
 ldflags: switch platform-config.os-base [
     'OSX [
-        "-framework CoreFoundation"
+        ["-framework CoreFoundation"]
     ]
 ]
