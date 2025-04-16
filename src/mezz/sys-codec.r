db0d87271c715d9179f379d088448bbfaa53d876
Brian Dickens <brian@hostilefork.com>
2025-04-16 02:08:40 -0400

    Delete code using broken ELSE logic
    It seems R3C has some trouble with a certain application of ELSE.  The
    usage here is for something that is not needed (since R3C does not
    support the LIBRARY! type or other things needed for loading DLL
    extensions) so the easiest thing to do is just delete it.

--- src/mezz/sys-codec.r
+++ src/mezz/sys-codec.r
@@ -59,23 +59,6 @@ register-codec*: func [
 ]
 
 
-; Special import case for extensions:
-append system/options/file-types switch fourth system/version [
-    3 [
-        [%.rx %.dll extension] ; Windows
-    ]
-    2 [
-        [%.rx %.dylib %.so extension] ; OS X
-    ]
-    4
-    7 [
-        [%.rx %.so extension] ; Other Posix
-    ]
-] else [
-    [%.rx extension]
-]
-
-
 decode: function [
     {Decodes a series of bytes into the related datatype (e.g. image!).}
 
