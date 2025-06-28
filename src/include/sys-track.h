2cbf1fc0b53d16778f506470a27a0f42a322c141
Brian Dickens <brian@hostilefork.com>
2025-06-28 02:49:58 -0400

    Some reorganization of cell tracking header

--- src/include/sys-track.h
+++ src/include/sys-track.h
@@ -6,7 +6,7 @@
 //
 //=////////////////////////////////////////////////////////////////////////=//
 //
-// Copyright 2012-2021 Ren-C Open Source Contributors
+// Copyright 2012-2025 Ren-C Open Source Contributors
 //
 // See README.md and CREDITS.md for more information.
 //
@@ -26,62 +26,93 @@
 //
 
 
+//=//// TRACKING FUNCTION ////////////////////////////////////////////////=//
+//
+// In the C++ build, the TRACK() macro is polymorphic, and gives you the
+// same Cell* you passed in in terms of the type.  So if it was a Cell* it
+// will come back a Cell*, etc.
+//
+// (In the C build, Cell*, Value*, Element, OnStack(Element*), etc. are all
+// just Cell* pointers.)
+//
+// 1. It's currently possible to enable or disable tick counting in the
+//    Trampoline.  This is because while tick counting is very useful even
+//    in release builds, it isn't free...and some configurations might not
+//    want to pay for counting them.  If not enabled, the tick will be 0.
+//
 #if DEBUG_TRACK_EXTEND_CELLS
 
-    #define Touch_Cell(c) \
-        ((c)->touch = TICK)
-
-    #define Touch_Cell_If_Debug(c) Touch_Cell(c)
+  POSSIBLY(TRAMPOLINE_COUNTS_TICKS);  // might not be enabled [1]
 
   #if CPLUSPLUS_11
     template<typename T>
     INLINE T Track_Cell_Debug(
-        T v,  // polymorphism allows Cell* or Value* or OnStack(Value*)
+        T cell,
         const char *file,
         int line
     ){
-        v->file = file;
-        v->line = line;
-        v->tick = TICK;
-        v->touch = 0;
-        return v;
+        cell->file = file;
+        cell->line = line;
+        cell->tick = TICK;  // TICK is 0 if not TRAMPOLINE_COUNTS_TICKS [1]
+        cell->touch = 0;
+        return cell;
     }
   #else
     INLINE Cell* Track_Cell_Debug(
-        Cell* v,
+        Cell* cell,
         const char *file,
         int line
     ){
-        v->file = file;
-        v->line = line;
-        v->tick = TICK;
-        v->touch = 0;
-        return cast(Value*, v);
+        cell->file = file;
+        cell->line = line;
+        cell->tick = TICK;  // TICK is 0 if not TRAMPOLINE_COUNTS_TICKS [1]
+        cell->touch = 0;
+        return cell;
     }
   #endif
+#endif
 
-    // NOTE: There is no guarantee of evaluation order of function arguments
-    // in C.  So if there's code like:
-    //
-    //    #define Init_Logic(out,flag)  Init_Logic_Core(TRACK(out), (flag))
-    //
-    // The tracking information may be put in the Cell* before* or *after*
-    // the right hand side is evaluated.  So imagine something like:
-    //
-    //     Init_Logic(OUT, not Cell_Logic(OUT));
-    //
-    // So TRACK() can't do anything that would corrupt the release-build-bits
-    // of `out`, since it might run first.  This is why the tracking info is
-    // fully separate, and doesn't try to exploit that not all cell types use
-    // all bits to hide more information.
-    //
-    #define TRACK(v) \
-        Track_Cell_Debug((v), __FILE__, __LINE__)
 
+//=//// CELL "TOUCH" TICK MONITORING //////////////////////////////////////=//
+//
+// The 4th slot in the tracking information is used for another tick value,
+// called "touch".  This can be used at arbitrary points in debugging to
+// indicate the last time the Cell underwent a change being studied.
+//
+#if DEBUG_TRACK_EXTEND_CELLS && TRAMPOLINE_COUNTS_TICKS
+    #define Touch_Cell(cell) \
+        ((cell)->touch = TICK)
+
+    #define Touch_Cell_If_Debug(cell) \
+        Touch_Cell(cell)
 #else
+    #define Touch_Cell(cell) \
+        STATIC_FAIL(need_DEBUG_TRACK_EXTEND_CELLS_and_TRAMPOLINE_COUNTS_TICKS)
 
-    #define Touch_Cell_If_Debug(c) NOOP
+    #define Touch_Cell_If_Debug(cell)  NOOP
+#endif
 
-    #define TRACK(v) (v)
 
+//=//// TRACK MACRO ///////////////////////////////////////////////////////=//
+//
+// 1. There is no guarantee of evaluation order of function arguments in C.
+//    So if there's code like:
+//
+//      #define Init_Logic(out,flag)  Init_Logic_Core(TRACK(out), (flag))
+//
+//    The tracking information may be put in the Cell* before* or *after* the
+//    second argument is evaluated.  So imagine something like:
+//
+//      Init_Logic(OUT, not Cell_Logic(OUT));
+//
+//    So TRACK() can't do anything that would corrupt the release-build-bits
+//    of `out`, since it might run first.  This is why the tracking info is
+//    fully separate, and doesn't try to exploit that not all cell types use
+//    all bits to hide more information.
+//
+#if DEBUG_TRACK_EXTEND_CELLS
+    #define TRACK(cell) /* see important note [1] above! */ \
+        Track_Cell_Debug((cell), __FILE__, __LINE__)
+#else
+    #define TRACK(cell)  (cell)
 #endif
