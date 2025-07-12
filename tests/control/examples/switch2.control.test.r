4e0f937d4e062c3719a50455ef213414af345b61
Brian Dickens <brian@hostilefork.com>
2025-07-12 04:30:16 -0400

    Fix SWITCH2 demo
    Needed to use UNSET? instead of trash? to find slots to fill, now
    that trash is no longer used for unspecialized slots.
    
    Before (^result: ...) assignments were legal directly, this used
    ([^result]: ...), but the change of VOID to being an empty pack makes
    the old form not work for VOID, because it tries to unpack an empty
    array.  So interesting edge case there, where the new form works and
    the workaround no longer does.
    
    Went ahead and switched away from using YES/NO style booleans to just
    using okay and null, because it's less hassle--yes and no should only
    probably be used when you're trying to put things in blocks and
    dialect them.

--- tests/control/examples/switch2.control.test.r
+++ tests/control/examples/switch2.control.test.r
@@ -10,27 +10,27 @@ switch2: func [
     value [any-stable?]
     cases [block!]
     :multi
-    <local> more found result' condition branch
+    <local> more found ^result condition branch
 ][
-    found: 'no
-    [^result']: void
-    more: 'yes
+    found: null
+    ^result: void
+    more: okay
 
     return parse cases [cycle [
-        while :(yes? more) [
+        while cond (more) [
             ; Find next condition clause, or break loop if we hit => or end
             ;
             condition: *in* between <here> any [
-                ['| (more: 'yes)]
-                ['=> (more: 'no)]
-                [<end> accept (^result')]
+                ['| (more: okay)]
+                ['=> (more: null)]
+                [<end> accept (^result)]
             ]
 
             any [
                 ; If we've already found a condition that matched, skip any
                 ; following alternates.
                 ;
-                [cond (yes? found) (log ["Skipping condition:" mold condition])]
+                [cond (found) (log ["Skipping condition:" mold condition])]
 
                 ; Otherwise, try building a frame for the condition.  If the
                 ; frame is incomplete, slip switch value into antiform ~ slot
@@ -39,14 +39,14 @@ switch2: func [
 
                     let f: make frame! make varargs! condition
                     for-each [key ^val] f [
-                        if trash? ^val [
+                        if unset? $val [
                             f.(key): value
                             break
                         ]
                     ]
                     log ["Built frame for condition:" mold f]
 
-                    if yes? found: to-yesno eval f [
+                    if found: did eval f [
                         log "Result was truthy, skipping to branch!"
                     ] else [
                         log "Result was falsey, seeking next condition."
@@ -55,7 +55,7 @@ switch2: func [
             ]
         ]
 
-        (more: 'yes)
+        (more: okay)
 
         branch: [
             *in* block!
@@ -67,14 +67,14 @@ switch2: func [
         any [
             ; If we didn't find a matching condition, skip over this branch.
             ;
-            [cond (no? found) (log ["Skipping branch:" mold branch])]
+            [cond (not found) (log ["Skipping branch:" mold branch])]
 
-            ; Otherwise, run the branch.  Keep going if we are using /MULTI,
+            ; Otherwise, run the branch.  Keep going if we are using :MULTI,
             ; else return whatever that branch gives back.
             (
-                ^result: if ok (branch)  ; IF semantics for null, void
+                ^result: if ok (branch)  ; branch semantics for null
                 if not multi [return ^result]
-                found: 'no
+                found: null
             )
         ]
     ]]
